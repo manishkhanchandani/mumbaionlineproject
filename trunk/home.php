6ea38aaa70f87d5de8ad60faf69ddb02b6ef083c
naveenkhanchandani <naveenkhanchandani@aedefa9c-ca13-11dd-a2c4-6dc3ad32ce87>
2008-12-15 03:15:14 +0000

    main

---
+++ trunk/home.php
@@ -0,0 +1,42 @@
+<?php
+include_once('start.php');
+
+// FUNCTIONALITY
+$j = md5($_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']);
+$feed = checkcache($j);
+if(!$feed) {
+	include_once('./Classes/simplepie.php');
+	$simplepie = new SimplePie;
+	$url = $_GET['url'];
+	if(!$url) 
+		$url = 'http://www.google.com/news?sourceid=navclient-ff&rlz=1B3GGGL_enIN279IN280&hl=en&ned=us&q=mumbai&ie=UTF-8&nolr=1&output=rss';
+	else 
+		$url = urldecode($url);
+	$simplepie->set_feed_url($url);
+	$simplepie->init();
+	$dig_feed = $simplepie;
+	$feed = "<ul>";
+	if($dig_feed) {
+		foreach($dig_feed->get_items() as $item) {
+			$feed .= "<li><a href='" .$item->get_link() . "' target='_blank'>" . $item->get_title() . "</a><br />";
+			$desc = str_replace("<a ", "<a target='_blank' ", $item->get_description());
+			$feed .= $desc."
+			</li>";
+		}
+	}
+	$feed .= "</ul>";
+	cachefunction($j, $feed);
+	$cache = 'no cache';
+} else {
+	$cache = 'cache';
+}
+?>
+<h1>Welcome to MumbaiOnline.Org.In</h1>
+<?php 
+echo $feed;
+?><!-- #BeginLibraryItem "/Library/endtime.lbi" --><?php
+$TIMEEND = microtime(true);
+$time = $TIMEEND - $TIMESTART;
+
+echo "<br /><br /><br /><br /><div align='right'><b>Time To Load:</b> $time seconds ($cache)</div>";
+?><!-- #EndLibraryItem --><p>&nbsp;</p>
